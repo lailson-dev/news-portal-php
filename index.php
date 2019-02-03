2552cdd0d246bcbaedeedf4af206175f65f35f08
Lailson Conceição <lailsoncv@hotmail.com>
2019-02-03 13:05:52 -0300

    Header da página

--- index.php
+++ index.php
@@ -6,11 +6,33 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<title>Portal de Notícias - PHP - Lailson Conceição</title>
 
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="assets/css/app.css">
+		
 	</head>
 	<body>
-		<h1>Hello, world!</h1>
+		<!-- Header Page -->
+		<header class="container py-3 news-header">
+			<div class="row flex-nowrap justify-content-between align-items-center">
+				<div class="col-4 pt-1">
+					<a class="text-muted" href="#">Assine</a>
+				</div>
+				<div class="col-4 text-center">
+					<p class="font-playfair text-dark">Online</p>
+				</div>
+				<div class="col-4 d-flex justify-content-end align-items-center">
+					<a class="text-muted" href="#">
+						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3" focusable="false" role="img"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
+					</a>
+					<a class="btn btn-sm btn-outline-secondary" href="#">Entrar</a>
+				</div>
+			</div>
+	  	</header>
+	  	<!-- End Header -->		
 
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
