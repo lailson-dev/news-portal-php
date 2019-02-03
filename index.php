a7d71dada4eaab9726bd0a56b05b8c37ee1a6d1f
Lailson Conceição <lailsoncv@hotmail.com>
2019-02-03 11:49:30 -0300

    Estrutura inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Portal de Notícias - PHP - Lailson Conceição</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	</head>
+	<body>
+		<h1>Hello, world!</h1>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="assets/js/bootstrap.bundle.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+	</body>
+</html>
