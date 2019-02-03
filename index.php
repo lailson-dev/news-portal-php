15a50f1e46211345ec54b48f51443f9d60ae12f0
Lailson Conceição <lailsoncv@hotmail.com>
2019-02-03 16:21:43 -0300

    Reutilização de códigos, conexão com banco de dados, página 404 e regra no arquivo htaccess

--- index.php
+++ index.php
@@ -1,61 +1,13 @@
-<?php include_once 'pages/header.php'; ?>
-<?php include_once 'pages/jumbotron.php'; ?>
+<?php
+include_once 'pages/header.php';
+include_once 'pages/jumbotron.php';
 
-<!-- Posts Page - Noticies -->
-<section class="container p-0 mb-5">
-	<h4 class="font-weight-bold">Notícia</h4>
-	<hr class="mb-4 mt-0">
-	<div class="row d-flex">				
-		<div class="col-sm-4">
-			<img src="assets/img/foto01.jpg" class="img-thumbnail" alt="">
-			<h5 class="my-3 font-weight-bold"><a href="#">Inscrições abertas para curso técnico gratuito em agronegócio</a></h5>
-			<p class="font-italic">Além da capital baiana, curso oferecido pelo Senar tem vagas para Barreiras e Juazeiro.</p>
-			<small class="badge badge-pill badge-secondary">Há 35 minutos</small>
-		</div>
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$url = str_replace('/projetos/news-portal', '', $uri);
 
-		<div class="col-sm-4">
-			<img src="assets/img/foto01.jpg" class="img-thumbnail" alt="">
-			<h5 class="my-3 font-weight-bold"><a href="#">Inscrições abertas para curso técnico gratuito em agronegócio</a></h5>
-			<p class="font-italic">Além da capital baiana, curso oferecido pelo Senar tem vagas para Barreiras e Juazeiro.</p>
-			<small class="badge badge-pill badge-secondary">Há 35 minutos</small>
-		</div>
+if($url == '/' || $url == '/index')
+	include_once 'pages/index.php';
+else
+	include_once '404.php';
 
-		<div class="col-sm-4">
-			<img src="assets/img/foto01.jpg" class="img-thumbnail" alt="">
-			<h5 class="my-3 font-weight-bold"><a href="#">Inscrições abertas para curso técnico gratuito em agronegócio</a></h5>
-			<p class="font-italic">Além da capital baiana, curso oferecido pelo Senar tem vagas para Barreiras e Juazeiro.</p>
-			<small class="badge badge-pill badge-secondary">Há 35 minutos</small>
-		</div>
-	</div>
-</section>
-<!-- End Posts Page - Noticies -->
-
-<!-- Posts Page - Noticies -->
-<section class="container mb-5 p-0">
-	<h4 class="font-weight-bold">Emprego</h4>
-	<hr class="mb-4 mt-0">
-	<div class="row d-flex">				
-		<div class="col-sm-4">
-			<img src="assets/img/foto02.jpeg" class="img-thumbnail" alt="">
-			<h5 class="my-3 font-weight-bold"><a href="#">Concessionária Litoral Norte abre processo seletivo</a></h5>
-			<p class="font-italic">A Concessionária Litoral Norte (CLN), uma empresa do grupo Invepar, está com processo seletivo aberto para o posto de gerente administrativo.</p>
-			<small class="badge badge-pill badge-secondary">Há 39 minutos</small>
-		</div>
-
-		<div class="col-sm-4">
-			<img src="assets/img/foto02.jpeg" class="img-thumbnail" alt="">
-			<h5 class="my-3 font-weight-bold"><a href="#">Concessionária Litoral Norte abre processo seletivo</a></h5>
-			<p class="font-italic">A Concessionária Litoral Norte (CLN), uma empresa do grupo Invepar, está com processo seletivo aberto para o posto de gerente administrativo.</p>
-			<small class="badge badge-pill badge-secondary">Há 39 minutos</small>
-		</div>
-
-		<div class="col-sm-4">
-			<img src="assets/img/foto02.jpeg" class="img-thumbnail" alt="">
-			<h5 class="my-3 font-weight-bold"><a href="#">Concessionária Litoral Norte abre processo seletivo</a></h5>
-			<p class="font-italic">A Concessionária Litoral Norte (CLN), uma empresa do grupo Invepar, está com processo seletivo aberto para o posto de gerente administrativo.</p>
-			<small class="badge badge-pill badge-secondary">Há 39 minutos</small>
-		</div>
-	</div>
-</section>
-<!-- End Posts Page - Noticies -->
-<?php include_once 'pages/footer.php'; ?>
+include_once 'pages/footer.php'; ?>
--- pages/header.php
+++ pages/header.php
@@ -1,3 +1,4 @@
+<?php require_once 'php/config/conexao.php'; ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
