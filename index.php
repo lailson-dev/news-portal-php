28a8cd477e6fcaa52eaa098a521e924b4012fba5
Lailson Conceição <lailsoncv@hotmail.com>
2019-02-03 13:44:04 -0300

    Formulário de pesquisa e finalização da index

--- index.php
+++ index.php
@@ -22,12 +22,16 @@
 					<a class="text-muted" href="#">Assine</a>
 				</div>
 				<div class="col-4 text-center">
-					<p class="font-playfair text-dark">Online</p>
+					<form action="">
+						<div class="input-group">
+							<input type="text" class="form-control" placeholder="Pesquisar..." aria-label="Recipient's username" aria-describedby="button-addon2">
+						    <div class="input-group-append">
+						    	<button class="btn btn-outline-secondary" type="submyt" id="button-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3" focusable="false" role="img"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg></button>
+						    </div>
+						</div>
+					</form>					
 				</div>
-				<div class="col-4 d-flex justify-content-end align-items-center">
-					<a class="text-muted" href="#">
-						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3" focusable="false" role="img"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
-					</a>
+				<div class="col-4 d-flex justify-content-end align-items-center">					
 					<a class="btn btn-sm btn-outline-secondary" href="#">Entrar</a>
 				</div>
 			</div>
@@ -75,7 +79,7 @@
 		<!-- End Posts Page - Noticies -->
 
 		<!-- Posts Page - Noticies -->
-		<section class="container p-0">
+		<section class="container mb-5 p-0">
 			<h4 class="font-weight-bold">Emprego</h4>
 			<hr class="mb-4 mt-0">
 			<div class="row d-flex">				
@@ -102,6 +106,43 @@
 			</div>
 		</section>
 		<!-- End Posts Page - Noticies -->
+		<!-- Footer Page -->
+		<section class="bg-gray">
+			<footer class="container news-footer p-0">
+				<div class="row">
+					<div class="col-sm-4">
+						<h5>Sobre nós</h5>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, ipsam reprehenderit optio, voluptatum officia quia culpa atque odio quasi quibusdam aperiam.</p>
+					</div>
+
+					<div class="col-sm-4">
+						<h5>Menu</h5>
+						<div class="list-unstyled btn-group-vertical btn-block btn-group-lg">
+							<a class="btn btn-link" href="#">Mundo</a>
+							<a class="btn btn-link" href="#">Política</a>
+							<a class="btn btn-link" href="#">Emprego</a>
+						</div>
+					</div>
+
+					<div class="col-sm-4">
+						<h5>Social</h5>
+						<div class="btn-group-vertical btn-block btn-group-lg">
+							<a class="btn btn-outline-primary" href="#"><i class="fa fa-facebook-official"></i> Facebook</a>
+							<a class="btn btn-outline-info" href="#"><i class="fa fa-facebook-official"></i> Twitter</a>
+							<a class="btn btn-outline-warning" href="#"><i class="fa fa-facebook-official"></i> Instagram</a>
+						</div>
+					</div>
+
+					<div class="col-12 text-center mt-5">
+						<blockquote class="blockquote text-center">
+						  <p class="mb-0">Devagarzinho eu chego lá :)</p>
+						  <footer class="blockquote-footer">Desenvolvido por <cite title="Source Title">Lailson Conceição</cite></footer>
+						</blockquote>
+					</div>
+				</div>
+			</footer>
+		</section>
+		<!-- End Footer Page -->
 		
 
 		<!-- Optional JavaScript -->
