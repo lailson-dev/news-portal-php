d156d00b2022e21ddcf6011118bbee0ad5dd592c
Lailson Conceição <lailsoncv@hotmail.com>
2019-02-03 13:06:27 -0300

    Chamada principal do portal

--- index.php
+++ index.php
@@ -32,7 +32,18 @@
 				</div>
 			</div>
 	  	</header>
-	  	<!-- End Header -->		
+	  	<!-- End Header -->
+
+	  	<!-- Main Page Call -->
+	  	<section class="container jumbotron py-3 bg-dark text-white">
+			<blockquote class="blockquote">
+				<h1 class="display-3 mb-5">Travessia tem fluxo tranquilo e saídas a cada 30 minutos</h1>
+				<p class="lead">Última embarcação deixa a capital às 19h30. No sentido contrário, última lancha sai às 18h30.</p>
+				<footer class="blockquote-footer"><cite title="Source Title">Há 35 minutos</cite></footer>
+			</blockquote>
+			<a class="btn btn-primary btn-lg" href="#" role="button">Ler mais...</a>
+		</section>
+		<!-- End Main Page Call -->
 
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
