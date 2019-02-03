0820425ce6e45e4a0bbbc086924a2bcc736db790
Lailson Conceição <lailsoncv@hotmail.com>
2019-02-03 13:12:05 -0300

    Listagem dos últimos três posts de empregos

--- index.php
+++ index.php
@@ -46,8 +46,8 @@
 		<!-- End Main Page Call -->
 
 		<!-- Posts Page - Noticies -->
-		<section class="container p-0">
-			<h4>Notícia</h4>
+		<section class="container p-0 mb-5">
+			<h4 class="font-weight-bold">Notícia</h4>
 			<hr class="mb-4 mt-0">
 			<div class="row d-flex">				
 				<div class="col-sm-4">
@@ -69,6 +69,35 @@
 					<h5 class="my-3 font-weight-bold"><a href="#">Inscrições abertas para curso técnico gratuito em agronegócio</a></h5>
 					<p class="font-italic">Além da capital baiana, curso oferecido pelo Senar tem vagas para Barreiras e Juazeiro.</p>
 					<small class="badge badge-pill badge-secondary">Há 35 minutos</small>
+				</div>
+			</div>
+		</section>
+		<!-- End Posts Page - Noticies -->
+
+		<!-- Posts Page - Noticies -->
+		<section class="container p-0">
+			<h4 class="font-weight-bold">Emprego</h4>
+			<hr class="mb-4 mt-0">
+			<div class="row d-flex">				
+				<div class="col-sm-4">
+					<img src="assets/img/foto02.jpeg" class="img-thumbnail" alt="">
+					<h5 class="my-3 font-weight-bold"><a href="#">Concessionária Litoral Norte abre processo seletivo</a></h5>
+					<p class="font-italic">A Concessionária Litoral Norte (CLN), uma empresa do grupo Invepar, está com processo seletivo aberto para o posto de gerente administrativo.</p>
+					<small class="badge badge-pill badge-secondary">Há 39 minutos</small>
+				</div>
+
+				<div class="col-sm-4">
+					<img src="assets/img/foto02.jpeg" class="img-thumbnail" alt="">
+					<h5 class="my-3 font-weight-bold"><a href="#">Concessionária Litoral Norte abre processo seletivo</a></h5>
+					<p class="font-italic">A Concessionária Litoral Norte (CLN), uma empresa do grupo Invepar, está com processo seletivo aberto para o posto de gerente administrativo.</p>
+					<small class="badge badge-pill badge-secondary">Há 39 minutos</small>
+				</div>
+
+				<div class="col-sm-4">
+					<img src="assets/img/foto02.jpeg" class="img-thumbnail" alt="">
+					<h5 class="my-3 font-weight-bold"><a href="#">Concessionária Litoral Norte abre processo seletivo</a></h5>
+					<p class="font-italic">A Concessionária Litoral Norte (CLN), uma empresa do grupo Invepar, está com processo seletivo aberto para o posto de gerente administrativo.</p>
+					<small class="badge badge-pill badge-secondary">Há 39 minutos</small>
 				</div>
 			</div>
 		</section>
