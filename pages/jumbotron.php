8363ea3b07185b155c541a920a99ba1705241d2d
Lailson Conceição <lailsoncv@hotmail.com>
2019-02-04 11:04:57 -0300

    Listagem da última matéria publicada

--- pages/jumbotron.php
+++ pages/jumbotron.php
@@ -1,9 +1,15 @@
+<?php
+$lastPost = $db->prepare('SELECT * FROM post ORDER BY id_post DESC LIMIT 1');
+$lastPost->execute();
+
+$lastPostResult = $lastPost->fetch();
+?>
 <!-- Main Page Call -->
 <section class="container jumbotron py-3 bg-dark text-white">
 	<blockquote class="blockquote">
-		<h1 class="display-3 mb-5">Travessia tem fluxo tranquilo e saídas a cada 30 minutos</h1>
-		<p class="lead">Última embarcação deixa a capital às 19h30. No sentido contrário, última lancha sai às 18h30.</p>
-		<footer class="blockquote-footer"><cite title="Source Title">Há 35 minutos</cite></footer>
+		<h1 class="display-3 mb-5"><?= $lastPostResult->title_post ?></h1>
+		<p class="lead"><?= $lastPostResult->subtitle_post ?></p>
+		<footer class="blockquote-footer"><cite title="Source Title"><?= date('d/m/Y h:s', strtotime($lastPostResult->date_post)) ?></cite></footer>
 	</blockquote>
 	<a class="btn btn-primary btn-lg" href="#" role="button">Ler mais...</a>
 </section>
