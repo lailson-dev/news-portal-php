437740c1b81ed42e0041c436ab3ba6164aca5717
Lailson Conceição <lailsoncv@hotmail.com>
2019-02-04 11:17:08 -0300

    Remoção de caracteres especiais na string e linkagem de url para leitura na página Materia

--- pages/jumbotron.php
+++ pages/jumbotron.php
@@ -3,6 +3,14 @@ $lastPost = $db->prepare('SELECT * FROM post ORDER BY id_post DESC LIMIT 1');
 $lastPost->execute();
 
 $lastPostResult = $lastPost->fetch();
+ 
+$string = strtr(utf8_decode($lastPostResult->title_post),
+        utf8_decode('ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ'),
+                 'SOZsozYYuAAAAAAACEEEEIIIIDNOOOOOOUUUUYsaaaaaaaceeeeiiiionoooooouuuuyy');
+ 
+$string = str_replace(',', '', $string);
+$string = str_replace('.', '', $string);
+$string = str_replace(' ', '-', $string);
 ?>
 <!-- Main Page Call -->
 <section class="container jumbotron py-3 bg-dark text-white">
@@ -11,6 +19,6 @@ $lastPostResult = $lastPost->fetch();
 		<p class="lead"><?= $lastPostResult->subtitle_post ?></p>
 		<footer class="blockquote-footer"><cite title="Source Title"><?= date('d/m/Y h:s', strtotime($lastPostResult->date_post)) ?></cite></footer>
 	</blockquote>
-	<a class="btn btn-primary btn-lg" href="#" role="button">Ler mais...</a>
+	<a class="btn btn-primary btn-lg" href="materia?title=<?= $string ?>&id=<?= $lastPostResult->id_post ?>" role="button">Ler mais...</a>
 </section>
 <!-- End Main Page Call -->
